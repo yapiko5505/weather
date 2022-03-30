333bab576e67325e3594d61b71f8d04d987958a4
yapiko <bloomingjewels7021@gmail.com>
2022-03-30 15:27:45 +0900

    weather1

---
+++ common/script2.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const now = new Date();
+const year = now.getFullYear();
+const month = now.getMonth();
+const date = now.getDate();
+const hour = now.getHours();
+const min = now.getMinutes();
+
+const output = `${year}/${month + 1}/${date} ${hour}:${min}`;
+document.getElementById('time').textContent = output;
